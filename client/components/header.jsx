35710ae3b43a61ccfef680d90ddf752a8fe2a5a6
Mansor Almossa <malmossa@gmail.com>
2020-10-01 10:12:12 -0700

    updating database table and the header

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,10 +3,9 @@ import React from 'react';
 function Header(props) {
   const cartText = props.cartItemCount === 1 ? 'item' : 'items';
   return (
-    <nav className="navbar navbar-light bg-dark">
+    <nav className="navbar navbar-light bg-info">
       <div className="navbar-brand text-white ml-5" href="#">
-        <img src="images/doller.png" width="30" height="30" className="d-inline-block align-top mr-1" />
-          Wicked Sales
+        <img src="images/logo.png" width="200" height="50" className="d-inline-block align-top mr-1" />
       </div>
       <div onClick={() => props.setView('cart', {})} className="text-white mr-5 cart-div">
         <span className="mr-2">{`${props.cartItemCount} ${cartText}`}</span>
